9ff9e3b8938337bc2cf4b40e8ae689fa65e4660c
garry.afrialdy <garry.afrialdy@indosiar.com>
2023-04-15 20:04:25 +0700

    perbaiki onclik

--- static/onclic.js
+++ static/onclic.js
@@ -1,5 +1,7 @@
-function testConnection() {
+function testConnection() 
+{
     var database_name = document.getElementById("database_name").value;
-    var host = document.getElementById("host_name").value;
-    var username = document.getElementById("username").
-    var password = document.getElementById("password").
+    var host_name = document.getElementById("host_name").value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+}
